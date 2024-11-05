4eb9e5566c41bcde877526ee8df3e64b3434b6d8
Jannassl <Jan.nassling@metropolia.fi>
2024-11-05 21:31:23 +0200

    All gucci

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -1,30 +1,41 @@
-const etusivuBtn = document.querySelector('#etusivuBtn');
-
-etusivuBtn.addEventListener('click', () => {
-    window.location.href = 'etusivu.html';
-});
-
-const yritysBtn = document.querySelector('#yritysBtn');
-
-yritysBtn.addEventListener('click', () => {
-    window.location.href = 'yritys.html'
-});
-
-document.getElementById('hamburgerMenu').addEventListener('click', function(event) {
-    event.stopPropagation();
-    const dropdownMenu = document.getElementById('hamburgerDropdownMenu');
-    if (dropdownMenu.style.display === 'block') {
-        dropdownMenu.style.display = 'none';
-    } else {
-        dropdownMenu.style.display = 'block';
+document.addEventListener('DOMContentLoaded', () => {
+    const etusivuBtn = document.querySelector('#etusivuBtn');
+    if (etusivuBtn) {
+        etusivuBtn.addEventListener('click', () => {
+            window.location.href = 'etusivu.html';
+        });
     }
-});
 
-document.addEventListener('click', function(event) {
-    const dropdownMenu = document.getElementById('hamburgerDropdownMenu');
-    const isClickInside = dropdownMenu.contains(event.target) || document.getElementById('hamburgerMenu').contains(event.target);
-
-    if (!isClickInside) {
-        dropdownMenu.style.display = 'none';
+    const yritysBtn = document.querySelector('#yritysBtn');
+    if (yritysBtn) {
+        yritysBtn.addEventListener('click', () => {
+            window.location.href = 'yritys.html';
+        });
     }
+
+    const huoltoKorttiBtn = document.querySelector('#huoltoKortti');
+    if (huoltoKorttiBtn) {
+        huoltoKorttiBtn.addEventListener('click', () => {
+            window.location.href = 'huoltotyot.html';
+        });
+    }
+
+    document.getElementById('hamburgerMenu').addEventListener('click', function(event) {
+        event.stopPropagation();
+        const dropdownMenu = document.getElementById('hamburgerDropdownMenu');
+        if (dropdownMenu.style.display === 'block') {
+            dropdownMenu.style.display = 'none';
+        } else {
+            dropdownMenu.style.display = 'block';
+        }
+    });
+
+    document.addEventListener('click', function(event) {
+        const dropdownMenu = document.getElementById('hamburgerDropdownMenu');
+        const isClickInside = dropdownMenu.contains(event.target) || document.getElementById('hamburgerMenu').contains(event.target);
+
+        if (!isClickInside) {
+            dropdownMenu.style.display = 'none';
+        }
+    });
 });
