1c6775da351cb1a6a9bfcc4e07c925a52532a947
Jannassl <Jan.nassling@metropolia.fi>
2024-11-05 19:36:33 +0200

    Nav bar responsiivisuutta ja huoltotoimenpide sivut

---
+++ src/js/buttons.js
@@ -0,0 +1,11 @@
+const etusivuBtn = document.querySelector('#etusivuBtn');
+
+etusivuBtn.addEventListener('click', () => {
+    window.location.href = 'etusivu.html';
+});
+
+const yritysBtn = document.querySelector('#yritysBtn');
+
+yritysBtn.addEventListener('click', () => {
+    window.location.href = 'yritys.html'
+})
