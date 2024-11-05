63128a8cb44e6c425620ebd87732e32cc7fe0ea3
Jannassl <Jan.nassling@metropolia.fi>
2024-11-05 20:26:32 +0200

    Responsiivisuus toimii

--- src/js/buttons.js
+++ src/js/buttons.js
@@ -8,4 +8,23 @@ const yritysBtn = document.querySelector('#yritysBtn');
 
 yritysBtn.addEventListener('click', () => {
     window.location.href = 'yritys.html'
-})
+});
+
+document.getElementById('hamburgerMenu').addEventListener('click', function(event) {
+    event.stopPropagation();
+    const dropdownMenu = document.getElementById('hamburgerDropdownMenu');
+    if (dropdownMenu.style.display === 'block') {
+        dropdownMenu.style.display = 'none';
+    } else {
+        dropdownMenu.style.display = 'block';
+    }
+});
+
+document.addEventListener('click', function(event) {
+    const dropdownMenu = document.getElementById('hamburgerDropdownMenu');
+    const isClickInside = dropdownMenu.contains(event.target) || document.getElementById('hamburgerMenu').contains(event.target);
+
+    if (!isClickInside) {
+        dropdownMenu.style.display = 'none';
+    }
+});
